8f1f81507da075fc5262b5506095cc56a6990601
JeremyVsquared <MacBookAir@jeremy.domain>
2012-01-27 19:30:25 -0500

    Fully established thunk integration with consideration for analytical evaluation

--- metacircularSchemeEvaluator.scm
+++ metacircularSchemeEvaluator.scm
@@ -84,7 +84,28 @@
 ;; lazy evaluation management
 (define (actual-value exp env)
   (force-it (eval exp env)))
-        
+(define (force-it obj)
+  (cond ((thunk? obj)
+         (let ((result (actual-value
+                        (thunk-exp obj)
+                        (thunk-env obj))))
+           (set-car! obj 'evaluated-thunk)
+           (set-car! (cdr obj) result)
+           (set-car! (cdr obj) '())
+           result))
+        ((evaluated-thunk? obj)
+         (thunk-value obj))
+        (else obj)))
+(define (delay-it exp env)
+  (list 'thunk exp env))
+(define (thunk? obj)
+  (tagged-list? obj 'thunk))
+(define (thunk-exp thunk) (cadr thunk))
+(define (thunk-env thunk) (caddr thunk))
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+(define (thunk-value evaluated-thunk) (cadr evaluated-thunk))
+
 ;; expression evaluation
 (define (apply procedure arguments)
   (cond ((primitive-procedure? procedure)
